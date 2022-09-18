2faeedb2e896a262fb5b52843c3b00e4a72e8603
franpessoa <frandevacc@gmail.com>
2022-09-18 17:36:59 -0300

    fix partitioning error

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ echo "Partitioning disks"
 sleep 2s
 parted /dev/sda mklabel gpt
 echo "criating esp partition"
-parted /dev/sda mkpart "EFI system partition" fat32 1MiB 401MiB
+parted /dev/sda mkpart "ESP" fat32 1MiB 401MiB
 parted /dev/sda set 1 esp on
 echo "creating swap"
-parted /dev/sda mkpart "swap partition" linux-swap 402MiB 5GiB
+parted /dev/sda mkpart "Swap" linux-swap 402MiB 5GiB
 echo "creating root"
-parted /dev/sda mkpart "root partition" btrfs 5001MiB 100%
+parted /dev/sda mkpart "Root" btrfs 5001MiB 100%
 
 echo "Formating partitions"
 sleep 2s
